d772bfb19b61aa9747d9505680f831da54af1dcf
Ty Richards <me@tyrichards.com>
2014-04-07 17:45:37 -0500

    Fix Header Link

--- wp-content/themes/Paradox/header.php
+++ wp-content/themes/Paradox/header.php
@@ -74,11 +74,11 @@
 					</nav>				
 				</section> <!-- section-navbar -->
 		    <div class="contact-nav hidden-xs">
-			    <a class="btn btn-primary btn" href="/schedule-inspection">
+			    <a class="btn btn-primary btn" href="tel:5555555555">
 						<i class="fa fa-phone fa-1x"></i>
 			    	(555) 555-5555
 			    </a> 
-			    <a class="btn btn-primary btn" href="/schedule-inspection">
+			    <a class="btn btn-primary btn" href="/locations">
 						<i class="fa fa-map-marker fa-1x"></i>			    	
 			    	Locations
 			    </a> 
